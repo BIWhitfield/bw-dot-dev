8d1e0bd9c688fe6da423301fc6b7b62cd0d1e64b
Ben Whitfield <ben.whitfield@ansgroup.co.uk>
2022-04-22 13:02:56 +0100

    initial commit

---
+++ src/bwDotDev/home/Home.jsx
@@ -0,0 +1,28 @@
+import Github from '../icons/GIthub';
+import Instagram from '../icons/Instagram';
+import LinkedIn from '../icons/LinkedIn';
+import './home.css';
+
+function Home() {
+  return (
+    <div className='home-container'>
+      <h1>Ben Whitfield</h1>
+      <h3>Lead Developer @ ANS Group, Manchester</h3>
+      <div className='links'>
+        <a href='https://github.com/BIWhitfield' target='_blank' rel="noreferrer">
+          <Github />
+        </a>
+
+        <a href='https://www.instagram.com/uptheoldkentroad/' target='_blank' rel="noreferrer">
+          <Instagram />
+        </a>
+
+        <a href='https://www.linkedin.com/in/biwhitfield/' target='_blank' rel="noreferrer">
+          <LinkedIn />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
