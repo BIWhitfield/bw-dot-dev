de5b2855e726e344f781db33238e99cb1d8ae9c9
Ben Whitfield <ben.whitfield@ansgroup.co.uk>
2023-07-24 16:47:48 +0100

    update job

--- src/bwDotDev/home/Home.jsx
+++ src/bwDotDev/home/Home.jsx
@@ -1,23 +1,35 @@
-import Github from '../icons/GIthub';
-import Instagram from '../icons/Instagram';
-import LinkedIn from '../icons/LinkedIn';
-import './home.css';
+import Github from "../icons/GIthub";
+import Instagram from "../icons/Instagram";
+import LinkedIn from "../icons/LinkedIn";
+import "./home.css";
 
 function Home() {
   return (
-    <div className='home-container'>
+    <div className="home-container">
       <h1>Ben Whitfield</h1>
-      <h3>Lead Developer @ ANS Group, Manchester</h3>
-      <div className='links'>
-        <a href='https://github.com/BIWhitfield' target='_blank' rel="noreferrer">
+      <h3>Tech Lead @ ANS Group, Manchester</h3>
+      <div className="links">
+        <a
+          href="https://github.com/BIWhitfield"
+          target="_blank"
+          rel="noreferrer"
+        >
           <Github />
         </a>
 
-        <a href='https://www.instagram.com/uptheoldkentroad/' target='_blank' rel="noreferrer">
+        <a
+          href="https://www.instagram.com/uptheoldkentroad/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <Instagram />
         </a>
 
-        <a href='https://www.linkedin.com/in/biwhitfield/' target='_blank' rel="noreferrer">
+        <a
+          href="https://www.linkedin.com/in/biwhitfield/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <LinkedIn />
         </a>
       </div>
